67d1e7d5132cd3ef5285799b423962379df84093
coderepo-deepak <sdpk.ks2@gmail.com>
2025-01-30 19:48:58 +0530

    New Deployment-KMS Key3

--- modules/NodeGroup/main.tf
+++ modules/NodeGroup/main.tf
@@ -1,17 +1,41 @@
-# Create a dedicated KMS key for EBS encryption
 resource "aws_kms_key" "eks_ebs_key" {
   description             = "KMS key for encrypting EBS volumes in EKS Node Group"
   enable_key_rotation     = true
   deletion_window_in_days = 7
 }
 
-# Attach an alias to the KMS key (optional but recommended)
 resource "aws_kms_alias" "eks_ebs_key_alias" {
   name          = "alias/eks-ebs-key"
   target_key_id = aws_kms_key.eks_ebs_key.key_id
 }
 
-# Modify your existing EKS Node Group to use the new KMS Key
+###############################
+
+resource "aws_launch_template" "eks_launch_template" {
+  name_prefix   = "${var.EKS_CLUSTER_NAME}-launch-template"
+  description   = "Launch template for EKS Node Group with encrypted EBS volumes"
+
+  block_device_mappings {
+    device_name = "/dev/xvda" # Default root volume for Amazon Linux 2
+
+    ebs {
+      volume_size           = 20
+      volume_type           = "gp3"
+      encrypted             = true
+      kms_key_id            = aws_kms_key.eks_ebs_key.arn
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.EKS_CLUSTER_NAME}-node"
+    }
+  }
+}
+
+###############################################
+
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = var.EKS_CLUSTER_NAME
   node_group_name = "${var.EKS_CLUSTER_NAME}-node_group"
@@ -34,18 +58,15 @@ resource "aws_eks_node_group" "node_group" {
   force_update_version = false
   instance_types = ["t3.small"]
 
+  launch_template {
+    id      = aws_launch_template.eks_launch_template.id
+    version = "$Latest"
+  }
+
   labels = {
     role = "${var.EKS_CLUSTER_NAME}-Node-group-role",
     name = "${var.EKS_CLUSTER_NAME}-node_group"
   }
 
   version = "1.27"
-
-  # Enable EBS encryption using the KMS key
-  encryption_config {
-    resources = ["secrets"]
-    provider {
-      key_arn = aws_kms_key.eks_ebs_key.arn
-    }
-  }
 }
